5bf2a3d55dc2922c829856ea78b5ce8e8de33e22
paigemjames <paigemjames98@gmail.com>
2023-10-20 14:39:56 -0400

    vite installed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const app = express();
-const express = require('express');
-const port = 3000;
+import config from './config/config.js'
+import app from './server/express.js'
+import mongoose from 'mongoose'
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, {
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+
+        console.log("Connected to the database!");
+
+    })
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database:${config.mongoUri}`)
+})
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to DressStore application.' });
+    res.json({ message: 'Welcome to DressStore application.' });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+
+app.listen(config.port, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on port %s.', config.port)
+})
